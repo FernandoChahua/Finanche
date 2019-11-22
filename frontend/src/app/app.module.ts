1f12cb3980c2c08d1446b61bddd3821cf1b97293
Diego <u201616874@upc.edu.pe>
2019-11-22 01:33:32 -0500

    commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,18 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutComponent } from './component/layout/layout.component';
-import { AppMaterialModule } from './app-material.module';
+import { AppMaterialModule } from './module/app-material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IConfig, NgxMaskModule } from 'ngx-mask';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import { HomeComponent } from './component/home/home.component';
 
 export let maskOptions: Partial<IConfig> | (() => Partial<IConfig>);
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
